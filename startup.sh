150f53a656de188b4b77277d433fa9e1a7ecadee
dark horse <asharma13524@gmail.com>
2023-12-22 16:48:20 -0500

    change to community snapshot, remove verbosity flag

--- startup.sh
+++ startup.sh
@@ -3,22 +3,22 @@ set -e
 
 DATA_DIR="/root/.aleo/storage/ledger-3"
 
-# echo "Removing any existing ledger data..."
-# rm -rf $DATA_DIR/*
+echo "Removing any existing ledger data..."
+rm -rf $DATA_DIR/*
 
-# echo "Downloading the blockchain data snapshot..."
-# wget -O /tmp/aleoledger.tar.gz https://ledger.aleo.network/aleoledger-1002442.tar.gz
+echo "Downloading the blockchain data snapshot..."
+wget -O /tmp/aleoledger.tar.gz https://aleo-snapshots.f5nodes.com/storage_1021525.tar.gz
 
-# echo "Extracting the blockchain data..."
-# mkdir -p $DATA_DIR
-# tar -xzf /tmp/aleoledger.tar.gz -C $DATA_DIR
+echo "Extracting the blockchain data..."
+mkdir -p $DATA_DIR
+tar -xzf /tmp/aleoledger.tar.gz -C $DATA_DIR
 mv $DATA_DIR/ledger-3-1002442/* $DATA_DIR/
 rm -rf $DATA_DIR/ledger-3-1002442
 
 ls ~/.aleo/storage/ledger-3
 
 echo "Cleaning up temporary files..."
-# rm -rf /tmp/aleoledger.tar.gz
+rm -rf /tmp/aleoledger.tar.gz
 
 echo "Starting snarkOS node..."
-exec cargo run --release -- start --metrics --nodisplay --client --verbosity 4
+exec cargo run --release -- start --metrics --nodisplay --client
