6fac5054afa89027cb74e303e389be22805c3ffd
dark horse <asharma13524@gmail.com>
2023-12-27 10:34:44 -0500

    add debug logs

--- startup.sh
+++ startup.sh
@@ -3,24 +3,42 @@ set -e
 
 DATA_DIR="/root/.aleo/storage/ledger-3"
 
+cleanup() {
+    echo "Cleaning up temporary files..."
+    rm -rf /tmp/aleoledger.tar.gz
+}
+
+trap cleanup EXIT
+
 echo "Removing any existing ledger data..."
 rm -rf $DATA_DIR/*
 
 echo "Downloading the blockchain data snapshot..."
 wget -O /tmp/aleoledger.tar.gz https://ledger.aleo.network/aleoledger-1002442.tar.gz
 
+echo "Checking the contents of the blockchain data snapshot..."
+tar -tzf /tmp/aleoledger.tar.gz
+
 echo "Extracting the blockchain data..."
 mkdir -p $DATA_DIR
 tar -xzf /tmp/aleoledger.tar.gz -C $DATA_DIR
-echo "ls on data dir"
+
+echo "Listing the data directory after extraction:"
 ls $DATA_DIR
-mv $DATA_DIR/ledger-3-1002442/* $DATA_DIR/
-rm -rf $DATA_DIR/ledger-3-1002442
 
+echo "Moving the extracted data to the data directory..."
+EXTRACTED_DIR=$(ls $DATA_DIR | grep 'ledger-3')
+if [ -d "$DATA_DIR/$EXTRACTED_DIR" ]; then
+    mv $DATA_DIR/$EXTRACTED_DIR/* $DATA_DIR/
+    echo "Removing the now-empty extracted directory..."
+    rm -rf $DATA_DIR/$EXTRACTED_DIR
+else
+    echo "Expected ledger directory not found in the extracted data."
+    exit 1
+fi
+
+echo "Listing the final contents of ~/.aleo/storage/ledger-3:"
 ls ~/.aleo/storage/ledger-3
-
-echo "Cleaning up temporary files..."
-rm -rf /tmp/aleoledger.tar.gz
 
 echo "Starting snarkOS node..."
 exec cargo run --release -- start --metrics --nodisplay --client
