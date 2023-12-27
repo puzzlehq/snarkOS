b167ef757bb575dd03b05124ec2b6ed7d5b90a70
dark horse <asharma13524@gmail.com>
2023-12-27 12:42:59 -0500

    attempt to use community snapshot

--- startup.sh
+++ startup.sh
@@ -14,7 +14,7 @@ echo "Removing any existing ledger data..."
 rm -rf $DATA_DIR/*
 
 echo "Downloading the blockchain data snapshot..."
-wget -O /tmp/aleoledger.tar.gz https://ledger.aleo.network/aleoledger-1002442.tar.gz
+wget -O /tmp/aleoledger.tar.gz https://aleo-snapshots.f5nodes.com/storage_1054016.tar.gz
 
 echo "Extracting the blockchain data..."
 mkdir -p $DATA_DIR
@@ -24,7 +24,7 @@ echo "Listing the data directory after extraction:"
 ls $DATA_DIR
 
 echo "Moving the extracted data to the data directory..."
-EXTRACTED_DIR="$DATA_DIR/storage/ledger-3-1002442"
+EXTRACTED_DIR="$DATA_DIR/storage/storage_1054016"
 if [ -d "$EXTRACTED_DIR" ]; then
     mv $EXTRACTED_DIR/* $DATA_DIR/
     echo "Removing the now-empty extracted directory..."
