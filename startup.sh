156a85232cab2480646f62223fa41599a1d78149
dark horse <asharma13524@gmail.com>
2023-12-27 14:33:43 -0500

    update extracted dir logging

--- startup.sh
+++ startup.sh
@@ -16,16 +16,22 @@ rm -rf $DATA_DIR/*
 echo "Downloading the blockchain data snapshot..."
 wget -O /tmp/aleoledger.tar.gz https://aleo-snapshots.f5nodes.com/storage_1054016.tar.gz
 
+echo "Checking contents of the tarball before extraction:"
+tar -tzf /tmp/aleoledger.tar.gz
+
 echo "Extracting the blockchain data..."
 mkdir -p $DATA_DIR
-tar -xzf /tmp/aleoledger.tar.gz -C $DATA_DIR
+tar -xzf /tmp/aleoledger.tar.gz -C $DATA_DIR || { echo "Extraction failed"; exit 1; }
 
 echo "Listing the data directory after extraction:"
 ls $DATA_DIR
 
-echo "Moving the extracted data to the data directory..."
 # Update the path based on your logs. This assumes the data is in $DATA_DIR/home/
 EXTRACTED_DIR="$DATA_DIR/home"
+echo "Checking what is inside the extracted folder:"
+ls $EXTRACTED_DIR
+
+echo "Moving the extracted data to the data directory..."
 if [ -d "$EXTRACTED_DIR" ]; then
     mv $EXTRACTED_DIR/* $DATA_DIR/
     echo "Removing the now-empty extracted directory..."
@@ -35,8 +41,8 @@ else
     exit 1
 fi
 
-echo "Listing the final contents of ~/.aleo/storage/ledger-3:"
-ls ~/.aleo/storage/ledger-3
+echo "Listing the final contents of $DATA_DIR:"
+ls $DATA_DIR
 
 echo "Starting snarkOS node..."
 exec cargo run --release -- start --metrics --nodisplay --client
