c030d1e7e27d8c780a56c7e10ab7486c98d172f5
dark horse <asharma13524@gmail.com>
2023-12-27 10:54:02 -0500

    update extracted dir

--- startup.sh
+++ startup.sh
@@ -16,9 +16,6 @@ rm -rf $DATA_DIR/*
 echo "Downloading the blockchain data snapshot..."
 wget -O /tmp/aleoledger.tar.gz https://ledger.aleo.network/aleoledger-1002442.tar.gz
 
-echo "Checking the contents of the blockchain data snapshot..."
-tar -tzf /tmp/aleoledger.tar.gz
-
 echo "Extracting the blockchain data..."
 mkdir -p $DATA_DIR
 tar -xzf /tmp/aleoledger.tar.gz -C $DATA_DIR
@@ -27,11 +24,11 @@ echo "Listing the data directory after extraction:"
 ls $DATA_DIR
 
 echo "Moving the extracted data to the data directory..."
-EXTRACTED_DIR=$(ls $DATA_DIR | grep 'ledger-3')
-if [ -d "$DATA_DIR/$EXTRACTED_DIR" ]; then
-    mv $DATA_DIR/$EXTRACTED_DIR/* $DATA_DIR/
+EXTRACTED_DIR="$DATA_DIR/storage/ledger-3-1002442"
+if [ -d "$EXTRACTED_DIR" ]; then
+    mv $EXTRACTED_DIR/* $DATA_DIR/
     echo "Removing the now-empty extracted directory..."
-    rm -rf $DATA_DIR/$EXTRACTED_DIR
+    rm -rf $EXTRACTED_DIR
 else
     echo "Expected ledger directory not found in the extracted data."
     exit 1
