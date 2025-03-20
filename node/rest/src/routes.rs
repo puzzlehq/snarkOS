74f94d59867878be90a064d89d7ac7e686edbd79
dark horse <asharma13524@gmail.com>
2025-03-20 11:58:44 -0400

    update revs

--- node/rest/src/routes.rs
+++ node/rest/src/routes.rs
@@ -39,7 +39,6 @@ pub(crate) struct StateProofsQuery {
     commitments: Option<Vec<String>>,
 }
 
-
 /// The query object for `get_mapping_value` and `get_mapping_values`.
 #[derive(Copy, Clone, Deserialize, Serialize)]
 pub(crate) struct Metadata {
@@ -273,8 +272,7 @@ impl<N: Network, C: ConsensusStorage<N>, R: Routing<N>> Rest<N, C, R> {
     pub(crate) async fn get_state_proofs_for_block(
         State(rest): State<Self>,
         Path(block_height): Path<u32>,
-        Query(params): Query<HashMap<String, Vec<String>>>
-        // Query(params): Query<StateProofsQuery>,
+        Query(params): Query<HashMap<String, Vec<String>>>, // Query(params): Query<StateProofsQuery>,
     ) -> Result<ErasedJson, RestError> {
         info!("block_height: {}", block_height);
 
@@ -285,27 +283,24 @@ impl<N: Network, C: ConsensusStorage<N>, R: Routing<N>> Rest<N, C, R> {
         // info!("commitments (raw): {:?}", params.commitments);
 
         let commitments: Vec<Field<N>> = params
-          .get("commitments")
-          .unwrap_or(&Vec::new())
-          .iter()
-          .map(|commitment| commitment.parse::<Field<N>>())
-          .collect::<Result<Vec<_>, _>>()?;
-
+            .get("commitments")
+            .unwrap_or(&Vec::new())
+            .iter()
+            .map(|commitment| commitment.parse::<Field<N>>())
+            .collect::<Result<Vec<_>, _>>()?;
 
         info!("parsed commitments: {:?}", commitments);
 
-        let result = tokio::task::spawn_blocking(move || {
-            rest.ledger.get_state_proofs_for_block(block_height, &commitments)
-        })
-        .await
-        .map_err(|err| RestError(format!("Failed to spawn blocking task - {err}")))?;
+        let result =
+            tokio::task::spawn_blocking(move || rest.ledger.get_state_proofs_for_block(block_height, &commitments))
+                .await
+                .map_err(|err| RestError(format!("Failed to spawn blocking task - {err}")))?;
 
         match result {
             Ok(proofs) => Ok(ErasedJson::pretty(proofs)),
             Err(err) => Err(RestError(format!("Unable to get state proofs - {err}"))),
         }
     }
-
 
     // GET /<network>/stateRoot/latest
     pub(crate) async fn get_state_root_latest(State(rest): State<Self>) -> ErasedJson {
