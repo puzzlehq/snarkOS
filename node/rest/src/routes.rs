1c41a6427f4dbe7d64cae8dad9e2716d11717db2
Luke Newman <5315645+lukenewman@users.noreply.github.com>
2025-05-12 14:45:10 -0400

    remove serde_qs in favor or axum's built-in query string support

--- node/rest/src/routes.rs
+++ node/rest/src/routes.rs
@@ -24,7 +24,7 @@ use indexmap::IndexMap;
 use rayon::prelude::*;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
-use serde_qs;
+
 /// The `get_blocks` query object.
 #[derive(Deserialize, Serialize)]
 pub(crate) struct BlockRange {
@@ -270,28 +270,15 @@ impl<N: Network, C: ConsensusStorage<N>, R: Routing<N>> Rest<N, C, R> {
         Ok(ErasedJson::pretty(rest.ledger.get_state_path_for_commitment(&commitment)?))
     }
 
-    // GET /<network>/statePath/{commitment}
+    // GET /<network>/stateProofsForBlock/{blockHeight}
     pub(crate) async fn get_state_proofs_for_block(
         State(rest): State<Self>,
         Path(block_height): Path<u32>,
-        req: Request<Body>,
+        Query(params): Query<StateProofsQuery>,
     ) -> Result<ErasedJson, RestError> {
-        // Extract query string
-        let query = req.uri().query().unwrap_or("");
-
-        // Create a non-strict config for serde_qs
-        let config = serde_qs::Config::new(5, false);
-
-        // Use serde_qs with non-strict mode to parse the query string
-        let params: StateProofsQuery =
-            config.deserialize_str(query).map_err(|e| RestError(format!("Failed to parse query parameters: {}", e)))?;
-
-        // Convert the query parameter to a Vec<String>
-        let commitments_str = params.commitments;
-
         // Parse the strings to the expected Field type.
         let commitments: Vec<Field<N>> =
-            commitments_str.iter().map(|s| s.parse::<Field<N>>()).collect::<Result<Vec<_>, _>>()?;
+            params.commitments.iter().map(|s| s.parse::<Field<N>>()).collect::<Result<Vec<_>, _>>()?;
 
         // Retrieve proofs in a blocking task.
         let proofs =
